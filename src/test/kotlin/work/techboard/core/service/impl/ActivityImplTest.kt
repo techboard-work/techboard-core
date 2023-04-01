5922cf275d18a3693691516ff3dbd066b005c373
Andrey Somov <public.somov@gmail.com>
2023-04-01 20:07:07 +0400

    Add info to import data

---
+++ src/test/kotlin/work/techboard/core/service/impl/ActivityImplTest.kt
@@ -0,0 +1,19 @@
+package work.techboard.core.service.impl
+
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+/**
+ * Unit tests for [ActivityImpl].
+ */
+class ActivityImplTest {
+    @BeforeEach
+    fun init() {
+
+    }
+    @Test
+    fun usersToUserDTOsShouldMapOnlyNonNullUsers() {
+        assertEquals(1, 1)
+    }
+}
