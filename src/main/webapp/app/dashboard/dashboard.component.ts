5b42105367cadfdc5167a2df67fb2281c79e45e8
zarro <zarro@busybee>
2023-05-22 14:13:02 +0200

    WIP: Adds modal with Add activity - concept.

--- src/main/webapp/app/dashboard/dashboard.component.ts
+++ src/main/webapp/app/dashboard/dashboard.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { IEnvironment } from '../entities/environment/environment.model';
 import { DashboardService } from './dashboard.service';
 import { Observable } from 'rxjs';
+import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'jhi-dashboard',
@@ -12,10 +13,33 @@ export class DashboardComponent implements OnInit {
   environments$: Observable<any>;
   environments: IEnvironment[];
 
+  closeResult: any = '';
+
   isOpen = true;
-  constructor(private dashboardSvc: DashboardService) {}
+  constructor(private dashboardSvc: DashboardService, private modalService: NgbModal) {}
 
   ngOnInit(): void {
     this.environments$ = this.dashboardSvc.getEnvironments();
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
+
+  open(content) {
+    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then(
+      result => {
+        this.closeResult = `Closed with: ${result}`;
+      },
+      reason => {
+        this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+      }
+    );
   }
 }
