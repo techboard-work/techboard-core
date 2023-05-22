ababaef72d67cb291a5f993550f9d9a9c84ba134
zarro <zarro@busybee>
2023-05-22 14:13:02 +0200

    WIP: Adds modal with Add activity - concept.

--- src/main/webapp/app/dashboard/dashboard.module.ts
+++ src/main/webapp/app/dashboard/dashboard.module.ts
@@ -5,9 +5,10 @@ import { dashboardRoutes } from './dashboard.route';
 import { DashboardComponent } from './dashboard.component';
 import { ActivitiesComponent } from './activities/activities.component';
 import { EnvironmentComponent } from './environment/environment.component';
+import { NgbDatepickerModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-  imports: [SharedModule, RouterModule.forChild(dashboardRoutes)],
+  imports: [SharedModule, RouterModule.forChild(dashboardRoutes), NgbDatepickerModule],
   declarations: [DashboardComponent, ActivitiesComponent, EnvironmentComponent],
 })
 export class DashboardModule {}
