017467b22d701c61c0c3f806f9f20675308b28d2
zarro <zarro@busybee>
2023-05-23 22:36:33 +0200

    Moves activity modal to its own component

--- src/main/webapp/app/dashboard/dashboard.module.ts
+++ src/main/webapp/app/dashboard/dashboard.module.ts
@@ -6,9 +6,10 @@ import { DashboardComponent } from './dashboard.component';
 import { ActivitiesComponent } from './activities/activities.component';
 import { EnvironmentComponent } from './environment/environment.component';
 import { NgbDatepickerModule } from '@ng-bootstrap/ng-bootstrap';
+import { AddActivityModalComponent } from './add-activity-modal/add-activity-modal.component';
 
 @NgModule({
   imports: [SharedModule, RouterModule.forChild(dashboardRoutes), NgbDatepickerModule],
-  declarations: [DashboardComponent, ActivitiesComponent, EnvironmentComponent],
+  declarations: [DashboardComponent, ActivitiesComponent, EnvironmentComponent, AddActivityModalComponent],
 })
 export class DashboardModule {}
